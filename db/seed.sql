8621f0a05cda73bf7c32a7235f304e68d25f66da
Anushka Tewari <165508067+Anushka404@users.noreply.github.com>
2024-12-17 00:13:33 +0530

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,6 @@
 -- Insert sample book entries
 INSERT INTO books (title, author, rating, notes, date_read)
 VALUES 
-    ('Atomic Habits', 'James Clear', 4, 'Great book on habit formation.', '2023-10-15'),
-    ('Six of Crows', 'Leigh Bardugo', 5, 'Six of Crows" follows a group of six outcasts and criminals from the city of Ketterdam who team up to pull off an impossible heist. The task is to break into the impenetrable Ice Court, a military stronghold in Fjerda, and steal a hostage who knows the secret to a dangerous drug called jurda parem, which amplifies Grisha powers to lethal levels.', '2024-01-10'),
-    ('Before the Coffee gets cold', 'Toshikazu Kawaguchi', 4, 'A slow-moving, but beautifully paced novel about time-travel and questioning the idea that if you were given an opportunity to visit the past, would you?', '2024-03-01');
+    ('Atomic Habits', 'James Clear', 4, 'Great book on habit formation.', 'https://covers.openlibrary.org/b/title/Atomic%20Habits-M.jpg' , '2023-10-15'),
+    ('Six of Crows', 'Leigh Bardugo', 5, 'Six of Crows" follows a group of six outcasts and criminals from the city of Ketterdam who team up to pull off an impossible heist. The task is to break into the impenetrable Ice Court, a military stronghold in Fjerda, and steal a hostage who knows the secret to a dangerous drug called jurda parem, which amplifies Grisha powers to lethal levels.', 'https://covers.openlibrary.org/b/title/Six%20of%20Crows-M.jpg', '2024-01-10'),
+    ('Before the Coffee gets cold', 'Toshikazu Kawaguchi', 4, 'A slow-moving, but beautifully paced novel about time-travel and questioning the idea that if you were given an opportunity to visit the past, would you?', 'https://covers.openlibrary.org/b/isbn/9781335430991-M.jpg', '2024-03-01');
