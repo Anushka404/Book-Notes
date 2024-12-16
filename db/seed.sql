89c91b9353474e5c7ac66c6737a36543bf344644
Anushka Tewari <165508067+Anushka404@users.noreply.github.com>
2024-12-17 00:14:48 +0530

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,5 @@
 -- Insert sample book entries
-INSERT INTO books (title, author, rating, notes, date_read)
+INSERT INTO books (title, author, rating, notes, cover_url, date_read)
 VALUES 
     ('Atomic Habits', 'James Clear', 4, 'Great book on habit formation.', 'https://covers.openlibrary.org/b/title/Atomic%20Habits-M.jpg' , '2023-10-15'),
     ('Six of Crows', 'Leigh Bardugo', 5, 'Six of Crows" follows a group of six outcasts and criminals from the city of Ketterdam who team up to pull off an impossible heist. The task is to break into the impenetrable Ice Court, a military stronghold in Fjerda, and steal a hostage who knows the secret to a dangerous drug called jurda parem, which amplifies Grisha powers to lethal levels.', 'https://covers.openlibrary.org/b/title/Six%20of%20Crows-M.jpg', '2024-01-10'),
